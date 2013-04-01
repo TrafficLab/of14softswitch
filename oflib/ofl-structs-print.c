ac764f6baa9bde451a8c0d6e985fcb842f7bcaff
Matt Ranostay <mranostay@vellosystems.com>
2013-04-01 11:27:32 -0700

    EXT262 Added missing table_mod print function changes
    Signed-off-by: Matt Ranostay <mranostay@vellosystems.com>

--- oflib/ofl-structs-print.c
+++ oflib/ofl-structs-print.c
@@ -942,9 +942,9 @@ ofl_structs_table_features_print(FILE *stream, struct ofl_table_features *s){
     fprintf(stream, "{table=\"");
     ofl_table_print(stream, s->table_id);  
     fprintf(stream, "\", name=\"%s\", "
-                          "metadata_match=\"%"PRIx64"\", metadata_write=\"%"PRIx64"\", config=\"%"PRIu32"\"," 
+                          "metadata_match=\"%"PRIx64"\", metadata_write=\"%"PRIx64"\", capabilities=\"%"PRIu32"\"," 
                           "max_entries=\"%"PRIu32"\"",
-                  s->name, s->metadata_match, s->metadata_write, s->config, s->max_entries);      
+                  s->name, s->metadata_match, s->metadata_write, s->capabilities, s->max_entries);      
     for(i =0; i < s->properties_num; i++){
         ofl_structs_table_properties_print(stream, s->properties[i]);    
     }    
